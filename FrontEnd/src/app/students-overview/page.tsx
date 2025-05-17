544ff4edbe439ed75408af458876490456aa0b6c
NataliaNogueira1 <natalianogueir2006@gmail.com>
2025-05-16 21:25:25 -0300

    feat: creation of feature/students-overview

---
+++ FrontEnd/src/app/students-overview/page.tsx
@@ -0,0 +1,9 @@
+import List from "@/components/List";
+
+export default function Alunos() {
+  return (
+    <div className="flex flex-col items-center">
+      <List />
+    </div>
+  );
+}
---
+++ FrontEnd/src/components/Close.tsx
@@ -0,0 +1,9 @@
+import { IoCloseOutline } from "react-icons/io5";
+
+export default function Close() {
+  return (
+    <div className="w-9 h-9">
+      <IoCloseOutline />
+    </div>
+  );
+}
