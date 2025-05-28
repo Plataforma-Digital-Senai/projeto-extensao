86a97462a9be2e5ec9f0a8cbb82dc16ba4f603d0
MarcelaMulato <marcela.mulato7@gmail.com>
2025-05-28 11:47:58 -0300

    feat: added details and responsivity

--- FrontEnd/src/app/my-profile-teacher/page.tsx
+++ FrontEnd/src/app/my-profile-teacher/page.tsx
@@ -3,11 +3,15 @@ import Sidebar from "@/components/sidebar";
 
 export default function ProfileTeacher() {
   return (
-    <div>
-      <Sidebar session_token_role="professor" />
+    <div className="grid grid-cols-[60px_1fr] md:grid-cols-[56px_1fr] min-h-screen">
+      <div className="w-20 md:w-64">
+        <Sidebar session_token_role="professor" />
+      </div>
 
-      <div className="flex flex-col items-center justify-center h-screen">
-        <FormTeacher />
+      <div className="flex items-center justify-center min-h-screen p-4 md:p-8">
+        <div className="w-full max-w-4xl">
+          <FormTeacher />
+        </div>
       </div>
     </div>
   );
--- FrontEnd/src/components/FormTeacher.tsx
+++ FrontEnd/src/components/FormTeacher.tsx
@@ -1,11 +1,14 @@
 "use client";
 
 import { useEffect, useState } from "react";
+import { Eye, EyeOff } from "lucide-react";
 
 const FormTeacher = () => {
   const [isEditing, setIsEditing] = useState(false);
   const [name, setName] = useState("Rodrigo Souza");
   const [password, setPassword] = useState("");
+  const [showCurrentPassword, setShowCurrentPassword] = useState(false);
+  const [showNewPassword, setShowNewPassword] = useState(false);
   const [formData, setFormData] = useState({ name, password: "" });
 
   const defaultImage = isEditing
@@ -40,29 +43,32 @@ const FormTeacher = () => {
   };
 
   return (
-    <div className="w-full max-w-5xl my-32 !bg-forms-teacher-bg rounded-xl shadow-xl py-8 px-8">
+    <div className="w-full max-w-5xl my-16 bg-forms-teacher-bg rounded-xl shadow-xl py-8 px-6 md:px-8">
       <form action="" className="">
         <div className="w-32 mx-auto mb-8">
-          <img
-            src={image}
-            alt="Imagem de perfil"
-            className="w-full h-full object-cover"
-          />
+          <label htmlFor="fileInput">
+            <img
+              src={image}
+              alt="Imagem de perfil"
+              className={`w-full h-full object-cover rounded-full cursor-pointer ${
+                isEditing ? "hover:opacity-80" : ""
+              }`}
+            />
+          </label>
 
           {isEditing && (
-            <div className="mt-2">
-              <input
-                type="file"
-                accept="image/*"
-                onChange={handleImageChange}
-                className="text-sm"
-              />
-            </div>
+            <input
+              id="fileInput"
+              type="file"
+              accept="image/*"
+              onChange={handleImageChange}
+              className="hidden"
+            />
           )}
         </div>
 
-        <div className="flex space-x-4 px-3">
-          <div className="w-1/2">
+        <div className="flex flex-col md:flex-row gap-4 px-3">
+          <div className="w-full md:w-1/2">
             <label>Nome:</label>
             {isEditing ? (
               <input
@@ -80,7 +86,7 @@ const FormTeacher = () => {
             )}
           </div>
 
-          <div className="w-1/2">
+          <div className="w-full md:w-1/2">
             <label>CPF:</label>
             <input
               className="border border-text bg-input-teacher-bg-a px-4 py-2 rounded-xl w-full"
@@ -91,8 +97,8 @@ const FormTeacher = () => {
           </div>
         </div>
 
-        <div className="flex space-x-4 px-3 py-3">
-          <div className="w-1/2">
+        <div className="flex flex-col md:flex-row gap-4 px-3 my-4">
+          <div className="w-full md:w-1/2">
             <label>E-mail:</label>
             <input
               className="border border-text bg-input-teacher-bg-a px-4 py-2 rounded-xl w-full"
@@ -102,7 +108,7 @@ const FormTeacher = () => {
             />
           </div>
 
-          <div className="w-1/2">
+          <div className="w-full md:w-1/2">
             <label>NI:</label>
             <input
               className="border border-text bg-input-teacher-bg-a px-4 py-2 rounded-xl w-full"
@@ -114,46 +120,63 @@ const FormTeacher = () => {
         </div>
 
         {isEditing && (
-          <div className="flex space-x-4 px-3 py-3">
-            <div className="w-1/2">
+          <div className="flex flex-col md:flex-row gap-4 px-3 my-4">
+            <div className="w-full md:w-1/2">
               <label className="block text-sm font-medium text-gray-700">
                 Senha Atual:
               </label>
-              <input
-                type="password"
-                className="border border-text bg-input-teacher-bg-a px-4 py-2 rounded-xl w-full"
-                value={password ? password : "********"}
-                disabled
-              />
+              <div className="relative">
+                <input
+                  type={showCurrentPassword ? "text" : "password"}
+                  className="border border-text bg-input-teacher-bg-a px-4 py-2 rounded-xl w-full"
+                  value={password ? password : "senhaAleatoria"}
+                  disabled
+                />
+                <span
+                  className="absolute right-3 top-1/2 -translate-y-1/2 cursor-pointer"
+                  onClick={() => setShowCurrentPassword((prev) => !prev)}
+                >
+                  {showCurrentPassword ? (
+                    <Eye size={18} />
+                  ) : (
+                    <EyeOff size={18} />
+                  )}
+                </span>
+              </div>
             </div>
 
-            <div className="w-1/2">
+            <div className="w-full md:w-1/2">
               <label className="block text-sm font-medium text-gray-700">
                 Nova Senha:
               </label>
-              <input
-                type="password"
-                className="border border-text bg-input-teacher-bg-a px-4 py-2 rounded-xl w-full"
-                value={formData.password}
-                onChange={(e) =>
-                  setFormData((prev) => ({
-                    ...prev,
-                    password: e.target.value,
-                  }))
-                }
-              />
+              <div className="relative">
+                <input
+                  type={showNewPassword ? "text" : "password"}
+                  className="border border-text bg-input-teacher-bg-a px-4 py-2 rounded-xl w-full"
+                  value={formData.password}
+                  onChange={(e) =>
+                    setFormData((p) => ({ ...p, password: e.target.value }))
+                  }
+                />
+                <span
+                  className="absolute right-3 top-1/2 -translate-y-1/2 cursor-pointer"
+                  onClick={() => setShowNewPassword((prev) => !prev)}
+                >
+                  {showNewPassword ? <Eye size={18} /> : <EyeOff size={18} />}
+                </span>
+              </div>
             </div>
           </div>
         )}
 
-        <div className="flex gap-4 mt-6">
+        <div className="flex justify-center mt-6">
           {isEditing ? (
             <>
               <div className="flex justify-center mx-auto">
                 <button
                   type="button"
                   onClick={handleSave}
-                  className="bg-btn-teacher-bg px-28 py-2 rounded-xl !text-white"
+                  className="bg-btn-teacher-bg px-16 sm:px-28 py-2 rounded-xl !text-white cursor-pointer"
                 >
                   Salvar
                 </button>
@@ -164,7 +187,7 @@ const FormTeacher = () => {
               <button
                 type="button"
                 onClick={handleEditClick}
-                className="bg-btn-teacher-bg px-28 py-2 rounded-xl !text-white"
+                className="bg-btn-teacher-bg px-16 sm:px-28 py-2 rounded-xl !text-white cursor-pointer"
               >
                 Editar
               </button>
